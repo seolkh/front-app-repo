2788940d790065a2de1980b162866ef8d7c9d0e3
encorecicd <repoher539@iconzap.com>
2022-06-15 05:33:52 +0000

    Add new blog version

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog
+            I want to go home.
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
